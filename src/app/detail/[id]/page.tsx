2a67d7a8c41fb8abf00ad9aea2a0d4a6dd7ac580
Douglas Brandão <douglasfrb@gmail.com>
2025-03-24 23:33:23 -0400

    feat: list both missing and found persons

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -9,6 +9,7 @@ export default async function missingPersonDetail({
 }) {
   const { id } = await params
   const person = await fetchMissingPersonDetail(id)
+  const isPersonFound = person.ultimaOcorrencia?.dataLocalizacao
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
@@ -33,8 +34,8 @@ export default async function missingPersonDetail({
           {person.ultimaOcorrencia?.ocorrenciaEntrevDesapDTO?.vestimentasDesaparecido || 'Não informado'}
         </span>
         <span className="text-white text-md">
-          <strong>Está vivo? </strong>
-          {person.sexo ? 'Sim' : 'Não'}
+          <strong>Mais informações: </strong>
+          {person.ultimaOcorrencia?.ocorrenciaEntrevDesapDTO?.informacao || 'Não informado'}
         </span>
         <span className="text-white text-md">
           <strong>Local do desaparecimento: </strong>
@@ -52,6 +53,26 @@ export default async function missingPersonDetail({
         alt="Default image for missing persons"
         className="justify-self-end"
       />
+      {
+        isPersonFound ?
+        <>
+          <div className="col-span-3 flex justify-center border-t-1 border-white pt-5 border-b-1 pb-5">
+            <h1 className="text-3xl uppercase font-extrabold">PESSOA LOCALIZADA</h1>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-white text-md">
+              <strong>Data da localização: </strong>
+              {new Intl.DateTimeFormat('pt-BR').format(new Date(person.ultimaOcorrencia?.dataLocalizacao))}
+            </span>
+            <span className="text-white text-md">
+              <strong>Vivo(a)? </strong>
+              {person.ultimaOcorrencia?.encontradoVivo ? 'Sim' : 'Não'}
+            </span>
+          </div>
+        </>
+        :
+        <div></div>
+      }
     </div>
   );
 }
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,15 +8,19 @@ interface Props {
 }
 
 export function Card({ person }: Props) {
+  const isPersonFound = person.ultimaOcorrencia?.dataLocalizacao
+
   return (
     <Link
       href={`/detail/${person.id}`}
       className="bg-white bg-cover bg-center bg-opacity-25 bg-[url(/brasao-pc.png)] grid grid-cols-3 gap-3 rounded-md w-[300px] p-5 content-between"
     >
       <div className="flex flex-col col-span-3 items-center">
-        <h2 className="font-extrabold uppercase text-3xl text-red-500">Desaparecido</h2>
-        <h6 className="font-bold uppercase text-base text-red-500">{person.nome}</h6>
-        <div className="border-1 w-full border-red-500"></div>
+        <h2 className={`font-extrabold uppercase text-3xl ${isPersonFound ? 'text-teal-800' : 'text-red-500'}`}>
+          {isPersonFound ? 'Encontrado' : 'Desaparecido'}
+        </h2>
+        <h6 className={`font-bold uppercase text-base text-center ${isPersonFound ? 'text-teal-800' : 'text-red-500'}`}>{person.nome}</h6>
+        <div className={`border-1 w-full ${isPersonFound ? 'border-teal-800' : 'border-red-500'}`}></div>
       </div>
       <div className="col-span-2 flex flex-col">
         <span className="text-black text-sm">
@@ -27,9 +31,13 @@ export function Card({ person }: Props) {
           <strong>Sexo: </strong>
           {person.sexo}
         </span>
-        <span className="text-black text-sm">
+        <span className="text-black text-sm text">
           <strong>Vestindo: </strong>
           {person.ultimaOcorrencia?.ocorrenciaEntrevDesapDTO?.vestimentasDesaparecido || 'Não informado'}
+        </span>
+        <span className="text-black text-sm italic">
+          <strong>Mais informações: </strong>
+          {person.ultimaOcorrencia?.ocorrenciaEntrevDesapDTO?.informacao || 'Não informado'}
         </span>
       </div>
       <div className="flex flex-col gap-1">
@@ -42,22 +50,44 @@ export function Card({ person }: Props) {
             alt="Default image for missing persons"
           />
         </div>
-        <div>
-          <p className="font-semibold text-sm text-black">Última vez visto</p>
-          <p className="text-xs text-black truncate">
-            <strong>Local: </strong>
-            {person.ultimaOcorrencia?.localDesaparecimentoConcat}
-          </p>
-          <p className="text-xs text-black">
-            <strong>Data: </strong>
-            {new Intl.DateTimeFormat('pt-BR').format(new Date(person.ultimaOcorrencia.dtDesaparecimento))}
-          </p>
-        </div>
+        {
+          isPersonFound ?
+            <div className="flex flex-col">
+              <span className="font-semibold text-sm text-black">Última ocorrência</span>
+              <span className="text-xs text-black truncate">
+                <strong>Vivo? </strong>
+                {person.ultimaOcorrencia?.encontradoVivo ? 'Sim' : 'Não'}
+              </span>
+              <span className="text-xs text-black">
+                <strong>Data: </strong>
+                {new Intl.DateTimeFormat('pt-BR').format(new Date(person.ultimaOcorrencia?.dataLocalizacao))}
+              </span>
+            </div>
+          :
+            <div className="flex flex-col">
+              <span className="font-semibold text-sm text-black">Última vez visto</span>
+              <span className="text-xs text-black truncate">
+                <strong>Local: </strong>
+                {person.ultimaOcorrencia?.localDesaparecimentoConcat}
+              </span>
+              <span className="text-xs text-black">
+                <strong>Data: </strong>
+                {new Intl.DateTimeFormat('pt-BR').format(new Date(person.ultimaOcorrencia.dtDesaparecimento))}
+              </span>
+            </div>
+        }
       </div>
       <div className="col-span-3">
-        <p className="text-sm text-black">
-          Se você tem qualquer informação sobre o paradeiro desta pessoa, entre em contato imediatamente com a Polícia Civil de Mato Grosso pelo telefone <strong>(65) 3613-5602</strong>.
-        </p>
+        {
+          isPersonFound ?
+          <p className="text-sm text-black">
+            Esta pessoa já foi localizada, mas ainda há muitos desaparecidos. Se você tem qualquer informação sobre o paradeiro de alguém que continua desaparecido, entre em contato imediatamente com a Polícia Civil de Mato Grosso pelo telefone <strong>(65) 3613-5602</strong>.
+          </p>
+          :
+          <p className="text-sm text-black">
+            Se você tem qualquer informação sobre o paradeiro desta pessoa, entre em contato imediatamente com a Polícia Civil de Mato Grosso pelo telefone <strong>(65) 3613-5602</strong>.
+          </p>
+         }
       </div>
     </Link>
   );
